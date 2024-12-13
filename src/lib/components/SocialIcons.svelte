747b3ccf8bd3ffa413ee01c5e513a749c7a66861
Josh Mosier <josh.r.mosier@gmail.com>
2024-12-12 19:06:33 -0500

    rss feed icon

--- src/lib/components/SocialIcons.svelte
+++ src/lib/components/SocialIcons.svelte
@@ -1,21 +1,25 @@
 <div class="flex items-center justify-center gap-6">
     <a href="https://www.linkedin.com/in/joshua-mosier/" class="text-gray-400 hover:text-purple-300 w-5" target="_blank" rel="noopener noreferrer">
-        <img src="/icons/linkedin.png" alt="LinkedIn" class="w-full h-auto object-contain w-5 opacity-70 hover:opacity-100" />
+        <img src="/icons/linkedin.png" alt="LinkedIn" class="h-auto object-contain opacity-70 hover:opacity-100" />
     </a>
 
     <a href="https://github.com/JoshuaMosier" class="text-gray-400 hover:text-purple-300 w-5" target="_blank" rel="noopener noreferrer">
-        <img src="/icons/github.png" alt="GitHub" class="w-full h-auto object-contain w-5 opacity-70 hover:opacity-100" />
+        <img src="/icons/github.png" alt="GitHub" class="h-auto object-contain opacity-70 hover:opacity-100" />
     </a>
 
     <a href="https://x.com/joshrmosier" class="text-gray-400 hover:text-purple-300 w-5" target="_blank" rel="noopener noreferrer">
-        <img src="/icons/x.png" alt="X" class="w-full h-auto object-contain w-5 opacity-70 hover:opacity-100" />
+        <img src="/icons/x.png" alt="X" class="h-auto object-contain opacity-70 hover:opacity-100" />
     </a>
 
     <a href="https://www.reddit.com/user/phootoon/" class="text-gray-400 hover:text-purple-300 w-5" target="_blank" rel="noopener noreferrer">
-        <img src="/icons/reddit.png" alt="Reddit" class="w-full h-auto object-contain w-5 opacity-70 hover:opacity-100" />
+        <img src="/icons/reddit.png" alt="Reddit" class="h-auto object-contain opacity-70 hover:opacity-100" />
     </a>
 
     <a href="https://www.thingiverse.com/photonicb/designs" class="text-gray-400 hover:text-purple-300 w-5" target="_blank" rel="noopener noreferrer">
-        <img src="/icons/thingiverse.png" alt="Thingiverse" class="w-full h-auto object-contain w-5 opacity-70 hover:opacity-100" />
+        <img src="/icons/thingiverse.png" alt="Thingiverse" class="h-auto object-contain opacity-70 hover:opacity-100" />
+    </a>
+
+    <a href="/rss.xml" class="text-gray-400 hover:text-purple-300 w-5" target="_blank" rel="noopener noreferrer">
+        <img src="/icons/rss.png" alt="RSS Feed" class="h-auto object-contain opacity-70 hover:opacity-100" />
     </a>
 </div> 
